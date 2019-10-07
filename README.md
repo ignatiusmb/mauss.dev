06e4e73a783ace4d87d7c5d6e595f3d1e014c4b9
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-10-07 15:58:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# [Portfolio of a Curious One](https://ignatiusmb.github.io)
+
+### Resources
+
+- [Font Awesome](https://fontawesome.com/)
+- [BlueSheets](https://github.com/ignatiusmb/bluesheets)
+- [Scramble](https://scramble.js.org)
---
+++ index.js
@@ -0,0 +1,34 @@
+;(async function() {
+  // Random Profile Picture
+  const random = scramble.random(0, 2)
+  let url = `https://cdn.imbagus.com/assets/images/avatar${random}.jpg`
+  let response = await fetch(url)
+  let data = await response.blob()
+  const profile = document.createElement('img')
+  profile.src = URL.createObjectURL(data)
+  document.getElementById('display-pic').appendChild(profile)
+
+  // Skills Logos
+  let ext
+  for (const logo of document.querySelectorAll('.logo')) {
+    const iconName = logo.querySelector('span').textContent.toLowerCase()
+    if (!logo.dataset.format) ext = 'svg'
+    else ext = logo.dataset.format
+    url = `https://cdn.imbagus.com/assets/icons/${iconName}.${ext}`
+    response = await fetch(url)
+    data = await response.blob()
+    const icon = document.createElement('img')
+    icon.src = URL.createObjectURL(data)
+    logo.appendChild(icon)
+  }
+})()
+
+const heading = document.querySelector('header h1')
+const instance = scramble(heading)
+instance.run()
+const setShadow = setInterval(() => {
+  if (instance.finished()) {
+    clearInterval(setShadow)
+    heading.style.textShadow = '6px 3px 9px rgba(0, 0, 0, 0.3)'
+  }
+}, 700)
