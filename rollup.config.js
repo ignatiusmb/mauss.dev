fcd128d00d3915d17fe46a3c89c320cd274b4f2e
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-12-29 14:43:34 +0700

    Add rollup-plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import replace from '@rollup/plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
@@ -34,6 +35,7 @@ export default {
         dedupe
       }),
       commonjs(),
+      json(),
 
       legacy &&
         babel({
@@ -83,7 +85,8 @@ export default {
       resolve({
         dedupe
       }),
-      commonjs()
+      commonjs(),
+      json()
     ],
     external: Object.keys(pkg.dependencies).concat(
       require('module').builtinModules || Object.keys(process.binding('natives'))
@@ -102,6 +105,7 @@ export default {
         'process.env.NODE_ENV': JSON.stringify(mode)
       }),
       commonjs(),
+      json(),
       !dev && terser()
     ],
 
