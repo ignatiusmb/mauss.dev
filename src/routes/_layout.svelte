ecf1171c637123708421ee796a7a4af6e0f9e6df
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-04-14 12:03:02 +0700

    Remove css import

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,9 +3,6 @@
   import Navigation from '../components/Navigation.svelte';
   import Footer from '../components/Footer.svelte';
   import ScrollTop from '../components/ScrollTop.svelte';
-
-  import '@ignatiusmb/aqua/lib/core.css';
-  import '@ignatiusmb/aqua/lib/main.css';
 </script>
 
 <Navigation {segment} />
