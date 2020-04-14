15b13661a895c99adf47436efa0103d98cee9dae
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-04-14 11:49:08 +0700

    routes/posts: add dynamic thumbnail fetching

--- src/routes/posts/index.json.ts
+++ src/routes/posts/index.json.ts
@@ -1,8 +1,11 @@
+import { Request, Response } from 'express';
+import { existsSync } from 'fs';
+import { join } from 'path';
 import { parseDir } from '../_parser';
 
-export function get(req, res) {
+export function get(req: Request, res: Response) {
   const DIR = 'content/posts';
-  const posts = parseDir(DIR, (cleanedFilename, frontMatter) => {
+  const posts = parseDir(DIR, (cleanedFilename: string, frontMatter: object) => {
     const [date, name] = cleanedFilename.split('.');
     frontMatter['slug'] = name;
     frontMatter['date'] = new Date(date);
@@ -11,8 +14,17 @@ export function get(req, res) {
     const month = frontMatter['date'].toLocaleDateString('default', { month: 'long' });
     const year = frontMatter['date'].getFullYear();
     frontMatter['pretty-date'] = `${weekday}, ${day} ${month} ${year}`;
+
+    const rootFolder = `${process.cwd()}/static`;
+    const imagePath = `uploads/${frontMatter['tags'][0]}/thumbnail/${frontMatter['slug']}`;
+    if (existsSync(join(rootFolder, `${imagePath}.png`))) {
+      frontMatter['image'] = `${imagePath}.png`;
+    } else if (existsSync(join(rootFolder, `${imagePath}.jpg`))) {
+      frontMatter['image'] = `${imagePath}.jpg`;
+    }
+
     return frontMatter;
-  }).filter(post => delete post.content);
+  }).filter((post) => delete post.content);
 
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(posts));
