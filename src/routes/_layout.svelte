d8ceb0e90121b2499646da8778bf43d713180dcd
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-04-24 22:28:27 +0700

    Upgrade rollup-json and add fontawesome

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,6 +3,10 @@
   import Navigation from '../components/Navigation.svelte';
   import Footer from '../components/Footer.svelte';
   import ScrollTop from '../components/ScrollTop.svelte';
+
+  import '@ignatiusmb/aqua/lib/core.css';
+  import '@ignatiusmb/aqua/lib/main.css';
+  import '@fortawesome/fontawesome-free/js/all.js';
 </script>
 
 <Navigation {segment} />
