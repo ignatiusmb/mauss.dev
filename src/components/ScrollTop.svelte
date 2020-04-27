d1c7704423a748003f6072e85f3b38545b500f34
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-04-28 03:17:21 +0700

    components: Refactor ScrollTop reactive height

--- src/components/ScrollTop.svelte
+++ src/components/ScrollTop.svelte
@@ -1,14 +1,13 @@
 <script>
   import { onMount } from 'svelte';
-
-  let y, height;
-  onMount(() => (height = document.body.scrollHeight / 3));
-
-  $: hide = y > height ? false : true;
+  let y, mounted;
+  onMount(() => (mounted = true));
+  $: height = mounted ? document.body.scrollHeight / 3 : y;
+  $: show = y > height;
 </script>
 
 <svelte:window bind:scrollY={y} />
-<span class:hide on:click={() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}>
+<span class:show on:click={() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}>
   <i class="fas fa-angle-double-up" />
 </span>
 
@@ -28,11 +27,11 @@
     font-size: 1.5em;
     color: var(--aqua-primary);
     background-color: rgba(0, 0, 0, 0.25);
-    transform: translateY(-40%);
-    transition: transform 200ms;
-  }
-  span.hide {
     transform: translateY(100%);
+    transition: transform var(--transition-duration);
+  }
+  span.show {
+    transform: translateY(-40%);
   }
   span:hover {
     background-color: rgba(0, 0, 0, 0.5);
