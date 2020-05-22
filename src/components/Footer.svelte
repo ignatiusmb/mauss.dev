decd1fd45301515f4391865cd36cde3d97127012
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-05-23 02:55:23 +0700

    components/Footer: Use LinkExt for icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,6 @@
 <script>
   import Icon from './independent/Icon.svelte';
+  import LinkExt from './independent/LinkExt.svelte';
   const size = 30;
 </script>
 
@@ -7,42 +8,42 @@
   <slot />
 
   <section class="social-links">
-    <a href="https://www.facebook.com/ignatiusmb" alt="">
+    <LinkExt href="https://www.facebook.com/ignatiusmb">
       <Icon name="facebook" {size} />
-    </a>
-    <a href="https://twitter.com/ignatiusmbs">
+    </LinkExt>
+    <LinkExt href="https://twitter.com/ignatiusmbs">
       <Icon name="twitter" {size} />
-    </a>
-    <a href="https://www.instagram.com/ignatiusmb">
+    </LinkExt>
+    <LinkExt href="https://www.instagram.com/ignatiusmb">
       <Icon name="instagram" {size} />
-    </a>
-    <a href="https://www.linkedin.com/in/ignatiusmb">
+    </LinkExt>
+    <LinkExt href="https://www.linkedin.com/in/ignatiusmb">
       <Icon name="linkedin" {size} />
-    </a>
-    <a href="https://github.com/ignatiusmb">
+    </LinkExt>
+    <LinkExt href="https://github.com/ignatiusmb">
       <Icon name="github" {size} />
-    </a>
-    <a href="https://gitlab.com/ignatiusmb">
+    </LinkExt>
+    <LinkExt href="https://gitlab.com/ignatiusmb">
       <Icon name="gitlab" {size} />
-    </a>
-    <!-- <a href="https://www.twitch.tv/magnawu">
+    </LinkExt>
+    <!-- <LinkExt href="https://www.twitch.tv/magnawu">
       <Icon name="twitch" {size} />
-    </a> -->
-    <a href="https://www.youtube.com/user/Ignapure">
+    </LinkExt> -->
+    <LinkExt href="https://www.youtube.com/user/Ignapure">
       <Icon name="youtube" {size} />
-    </a>
-    <!-- <a href="https://open.spotify.com/user/ignatiusmb">
+    </LinkExt>
+    <!-- <LinkExt href="https://open.spotify.com/user/ignatiusmb">
       <Icon name="spotify" {size} />
-    </a> -->
+    </LinkExt> -->
   </section>
 
   <section>
-    <p>Copyright &copy; 2017 — {new Date().getFullYear()} Ignatius Bagussuputra</p>
+    <p>Ignatius Bagussuputra &copy; 2017 — {new Date().getFullYear()}</p>
     <p>
       Made with ❤️ and passion using
-      <a href="https://svelte.dev/">Svelte</a>
-      &plus;
-      <a href="https://sapper.svelte.dev/">Sapper</a>
+      <LinkExt href="https://svelte.dev/">Svelte</LinkExt>
+      <Icon name="plus" size={15} />
+      <LinkExt href="https://sapper.svelte.dev/">Sapper</LinkExt>
     </p>
   </section>
 </footer>
@@ -67,31 +68,31 @@
     margin: auto;
     font-size: 2rem;
   }
-  .social-links a,
-  .social-links a:visited {
+  .social-links :global(a),
+  .social-links :global(a:visited) {
     transition: 0.15s;
     transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
     color: var(--fg-secondary-color);
   }
-  .social-links a:hover,
-  .social-links a:focus {
+  .social-links :global(a:hover),
+  .social-links :global(a:focus) {
     transition: 0.15s;
     transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
     color: crimson;
   }
-  .social-links a:hover:nth-child(odd),
-  .social-links a:focus:nth-child(odd) {
+  .social-links :global(a:hover:nth-child(odd)),
+  .social-links :global(a:focus:nth-child(odd)) {
     transform: rotate(6deg) scale(1.2) translateY(-20%);
   }
-  .social-links a:hover:nth-child(even),
-  .social-links a:focus:nth-child(even) {
+  .social-links :global(a:hover:nth-child(even)),
+  .social-links :global(a:focus:nth-child(even)) {
     transform: rotate(-6deg) scale(1.2) translateY(-20%);
   }
   section:last-of-type {
     display: grid;
     grid-template-rows: repeat(2, 1fr);
     gap: 0.5em;
-    font-size: 0.9rem;
+    font-family: 'Fira Code', 'Inconsolata', monospace;
   }
 
   @media only screen and (min-width: 480px) {
