413327febce3c855aae59708146a5cf5ce5ce0bb
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-05-24 22:18:07 +0700

    components/Siblings: improve styling and display

--- src/components/Siblings.svelte
+++ src/components/Siblings.svelte
@@ -1,14 +1,19 @@
 <script>
   export let base, prev, next;
-  import Link from './independent/Link.svelte';
 </script>
 
 <aside>
   {#if prev}
-    <Link href="{base}/{prev.slug}">&larr; {prev.title}</Link>
+    <a rel="prefetch" href="{base}/{prev.slug}" style="text-align: left">
+      <strong>&larr; Prev</strong>
+      <p>{prev.title}</p>
+    </a>
   {/if}
   {#if next}
-    <Link href="{base}/{next.slug}">{next.title} &rarr;</Link>
+    <a rel="prefetch" href="{base}/{next.slug}" style="text-align: right">
+      <strong>Next &rarr;</strong>
+      <p>{next.title}</p>
+    </a>
   {/if}
 </aside>
 
@@ -16,11 +21,37 @@
   aside {
     margin-top: 2em;
     display: grid;
+    grid-template-columns: 1fr;
+    border-radius: 0.5em;
+    border: 0.1em solid black;
   }
-  aside :global(a) {
-    width: 100%;
+  a {
+    display: grid;
+    grid-template-rows: auto 1fr;
+    color: inherit;
   }
-  aside :global(a:last-child) {
-    text-align: right;
+  a:nth-child(2) {
+    border-top: 0.1em solid black;
+  }
+  a:only-child {
+    grid-column: 1 / -1;
+  }
+  a strong,
+  a p {
+    padding: 0.2em 0.8em;
+    margin: 0;
+  }
+  strong {
+    border-bottom: 0.1em solid black;
+  }
+
+  @media only screen and (min-width: 600px) {
+    aside {
+      grid-template-columns: 1fr 1fr;
+    }
+    a:nth-child(2) {
+      border-top: none;
+      border-left: 0.1em solid black;
+    }
   }
 </style>
