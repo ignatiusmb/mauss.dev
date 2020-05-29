080fd69b427a5adb356d04879e3429d86f787e53
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-05-29 11:21:18 +0700

    Add about content and routes

---
+++ content/about.md
@@ -0,0 +1,9 @@
+---
+title: About
+---
+
+Hey there, I'm Ignatius Bagussuputra.
+
+I'm currently an CS undergraduate at the University of Indonesia.
+
+I'm quite a slow typer at around 65 WPM, perhaps I'm too spoiled by mobile keyboard autocomplete with type swiping and IntelliSense my code editor.
---
+++ src/routes/about.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+  export async function preload(page, session) {
+    return { post: await this.fetch('about.json').then(r => r.json()) };
+  }
+</script>
+
+<script>
+  export let post;
+  import PostHeader from '../components/PostHeader.svelte';
+  import PostArticle from '../pages/PostArticle.svelte';
+</script>
+
+<svelte:head>
+  <title>About &bull; IMB</title>
+</svelte:head>
+
+<PostHeader {post} segment="content" filename="about.md" />
+<PostArticle segment="content" filename="about.md">
+  <section>
+    {@html post.content}
+  </section>
+</PostArticle>
