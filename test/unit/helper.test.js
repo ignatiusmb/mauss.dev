d0731023e040a9cd3c26c2994e57b00f0d48cfd7
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-06-04 14:34:50 +0700

    test/unit/helper: Add lastWords test function

--- test/unit/helper.test.js
+++ test/unit/helper.test.js
@@ -1,34 +1,40 @@
 import * as helper from '../../src/utils/helper';
 
 describe('capitalize', () => {
-  it('convert first character to uppercase', () => {
-    expect(helper.capitalize('hello')).toBe('Hello');
-  });
-  it('convert first character to uppercase and lowercase the rest', () => {
-    expect(helper.capitalize('pAtRiCk', true)).toBe('Patrick');
-  });
+	it('convert first character to uppercase', () => {
+		expect(helper.capitalize('hello')).toBe('Hello');
+	});
+	it('convert first character to uppercase and lowercase the rest', () => {
+		expect(helper.capitalize('pAtRiCk', true)).toBe('Patrick');
+	});
 });
 
 describe('createPrettyDate', () => {
-  it('convert YYYY-MM-DD to a prettier format', () => {
-    expect(helper.createPrettyDate('2020-05-25')).toBe('Monday, 25 May 2020');
-  });
+	it('convert YYYY-MM-DD to a prettier format', () => {
+		expect(helper.createPrettyDate('2020-05-25')).toBe('Monday, 25 May 2020');
+	});
 });
 
 describe('isAbbreviated', () => {
-  it('true for word with less than 4 characters', () => {
-    expect(helper.isAbbreviated('ppl')).toBeTruthy();
-  });
-  it('true for word with length > 4 and prefixed with less than 3 chars', () => {
-    expect(helper.isAbbreviated('ppl2020')).toBeTruthy();
-  });
-  it('false for word with length > 4 and not prefixed with less than 3 chars', () => {
-    expect(helper.isAbbreviated('svelte')).toBeFalsy();
-  });
+	it('true for word with less than 4 characters', () => {
+		expect(helper.isAbbreviated('ppl')).toBeTruthy();
+	});
+	it('true for word with length > 4 and prefixed with less than 3 chars', () => {
+		expect(helper.isAbbreviated('ppl2020')).toBeTruthy();
+	});
+	it('false for word with length > 4 and not prefixed with less than 3 chars', () => {
+		expect(helper.isAbbreviated('svelte')).toBeFalsy();
+	});
+});
+
+describe('lastWords', () => {
+	it('must return last 2 words of text', () => {
+		expect(helper.lastWords(2, 'oh hello there')).toBe('hello there');
+	});
 });
 
 describe('splitAt', () => {
-  it('split word at specified index', () => {
-    expect(helper.splitAt(3, 'one:two')).toEqual(['one', 'two']);
-  });
+	it('split word at specified index', () => {
+		expect(helper.splitAt(3, 'one:two')).toEqual(['one', 'two']);
+	});
 });
