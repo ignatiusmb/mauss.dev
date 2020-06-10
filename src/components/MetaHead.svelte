48306939d374390c1f5795e18db6bfb658b66cfd
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-06-10 16:04:35 +0700

    components/MetaHead: Fix url to comply dynamic host

--- src/components/MetaHead.svelte
+++ src/components/MetaHead.svelte
@@ -3,13 +3,18 @@
   export let canonical = '';
   export let title, description;
   export let social = {};
-  $: canonical = `https://mauss.dev/${canonical}`;
+
+  import { stores } from '@sapper/app';
+  const { page } = stores();
+  $: host = $page.host;
+
+  $: url = `http${host.includes(':') ? '' : 's'}://${host}/${canonical}`;
 </script>
 
 <svelte:head>
   <title>{title} &bull; Mauss</title>
   {#if canonical}
-    <link rel="canonical" href={canonical} />
+    <link rel="canonical" href={url} />
   {/if}
   <meta name="author" content="Ignatius Bagussuputra" />
   {#if description}
@@ -21,10 +26,14 @@
     {/if}
     <meta property="og:title" content={post.title} />
     <meta property="og:type" content="article" />
-    <meta property="og:url" content={canonical} />
-    <meta property="og:description" content={post.description} />
-    <meta property="article:published_time" content={post.date} />
-    <meta property="article:modified_time" content={post.updated} />
+    <meta property="og:url" content={url} />
+    {#if post.description}
+      <meta property="og:description" content={post.description} />
+    {/if}
+    {#if post.date}
+      <meta property="article:published_time" content={post.date} />
+      <meta property="article:modified_time" content={post.updated} />
+    {/if}
     <meta property="article:author" content="Ignatius Bagussuputra" />
     <meta property="article:author" content="Ignatius Bagus" />
     {#if post.tags}
@@ -40,9 +49,11 @@
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@ignatiusmbs" />
       <meta name="twitter:creator" content="@ignatiusmbs" />
-      <meta name="twitter:url" content={canonical} />
+      <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={post.title} />
-      <meta name="twitter:description" content={post.description} />
+      {#if post.description}
+        <meta name="twitter:description" content={post.description} />
+      {/if}
     {/if}
   {/if}
   <meta property="og:site_name" content="Mauss" />
