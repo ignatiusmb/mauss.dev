9f290aa40c95e7dc5db1aeca9230bae94616fe0b
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-06-12 01:56:51 +0700

    components/Button: Remove left margin

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -20,9 +20,8 @@
   }
   span:hover,
   span:active {
-    margin-left: 1em;
     box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-    transform: scale(1.1);
+    transform: scale(1.05);
   }
   span :global(a) {
     padding: 0.5em 1em;
