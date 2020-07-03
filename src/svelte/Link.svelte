84fdc8e27ddf5f9d9f3b7403f31a6fbb2b7aa81c
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-03 23:20:36 +0700

    Rename independent folder to svelte

--- src/svelte/Link.svelte
+++ src/svelte/Link.svelte
@@ -1,16 +1,26 @@
 <script>
-  export let href;
+  export let href = '';
   export let inherit = true;
   export let invert = true;
+  export let label = null;
 </script>
 
-<a rel="preload" {href} class:inherit class:invert>
+<a
+  {href}
+  rel={href.includes('//') ? 'noopener noreferrer' : 'preload'}
+  aria-label={label}
+  class:disabled={!href}
+  class:inherit
+  class:invert>
   <slot />
 </a>
 
 <style>
   a {
     cursor: pointer;
+  }
+  a.disabled {
+    cursor: not-allowed;
   }
   a.inherit {
     color: inherit !important;
---
+++ src/svelte/Progressbar.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+  let y, innerHeight, mounted;
+  onMount(() => (mounted = true));
+  $: progress = (y / ((mounted ? document.body.scrollHeight : y) - innerHeight)) * 100;
+</script>
+
+<svelte:window bind:scrollY={y} bind:innerHeight />
+<div style="width:{progress > 99.5 ? 100 : progress}%" />
+
+<style>
+  div {
+    z-index: 3;
+    height: 0.25em;
+    position: fixed;
+    top: 0;
+    background-color: #990000;
+  }
+</style>
