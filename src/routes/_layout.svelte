4cd562da6167bf65dced0f1766a985d0d763c7fe
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-08 16:30:22 +0700

    Remove default Navigation from root page

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,34 +1,37 @@
 <script>
-  import IconList from '../svelte/IconList.svelte';
-  import ScrollTop from '../svelte/ScrollTop.svelte';
+	export let segment;
+	import IconList from '../svelte/IconList.svelte';
+	import ScrollTop from '../svelte/ScrollTop.svelte';
 
-  import Navigation from '../components/Navigation.svelte';
-  import Footer from '../components/Footer.svelte';
+	import Navigation from '../components/Navigation.svelte';
+	import Footer from '../components/Footer.svelte';
 
-  import '@ignatiusmb/aqua/lib/aqua.min.css';
-  import '../styles/fonts.css';
-  import '../styles/theme.css';
-  import '../styles/blog.css';
-  import '../styles/animation.css';
+	import '@ignatiusmb/aqua/lib/aqua.min.css';
+	import '../styles/fonts.css';
+	import '../styles/theme.css';
+	import '../styles/blog.css';
+	import '../styles/animation.css';
 
-  import { mobile } from '../stores';
-  let innerWidth;
-  $: $mobile = innerWidth < 600;
+	import { mobile } from '../stores';
+	let innerWidth;
+	$: $mobile = innerWidth < 600;
 </script>
 
 <svelte:window bind:innerWidth />
 <IconList />
 <ScrollTop />
 
-<Navigation mobile={$mobile} />
+{#if segment}
+	<Navigation mobile={$mobile} />
+{/if}
 <slot />
 <Footer mobile={$mobile} />
 
 <style>
-  :global(#sapper) {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    font-family: var(--aqua-default);
-  }
+	:global(#sapper) {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		font-family: var(--aqua-default);
+	}
 </style>
