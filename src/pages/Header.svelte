8a7ea6599aafdfead1076933d3cbeea4aa4d9637
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-10 20:44:16 +0700

    Align center the header metadata

--- src/pages/Header.svelte
+++ src/pages/Header.svelte
@@ -1,60 +1,61 @@
 <script>
-  export let path = null;
-  export let post;
-  import Edit from '../svelte/Edit.svelte';
-  import { lastWords } from '../utils/helper';
+	export let path = null;
+	export let post;
+	import Edit from '../svelte/Edit.svelte';
+	import { lastWords } from '../utils/helper';
 </script>
 
 <header>
-  <h1>{post['title']}</h1>
-  <small>
-    {#if post.date}
-      <span>
-        <time datetime={post.date}>{post['pretty-date']}</time>
-      </span>
-    {/if}
-    {#if post.updated && post.updated !== post.date}
-      <span>
-        <time datetime={post.updated}>Updated {lastWords(2, post['pretty-updated'])}</time>
-      </span>
-    {/if}
-    <span>{post['read-time']} min read</span>
-    {#if path}
-      <Edit {path} />
-    {/if}
-  </small>
+	<h1>{post['title']}</h1>
+	<small>
+		{#if post.date}
+			<span>
+				<time datetime={post.date}>{post['pretty-date']}</time>
+			</span>
+		{/if}
+		{#if post.updated && post.updated !== post.date}
+			<span>
+				<time datetime={post.updated}>Updated {lastWords(2, post['pretty-updated'])}</time>
+			</span>
+		{/if}
+		<span>{post['read-time']} min read</span>
+		{#if path}
+			<Edit {path} />
+		{/if}
+	</small>
 
-  <slot />
+	<slot />
 </header>
 
 <style>
-  header {
-    display: flex;
-    flex-direction: column;
-    word-wrap: break-word;
-    line-height: 1;
-    font-family: var(--aqua-heading);
-  }
+	header {
+		display: flex;
+		flex-direction: column;
+		word-wrap: break-word;
+		line-height: 1;
+		font-family: var(--aqua-heading);
+	}
 
-  h1 {
-    margin: 1em 0 0.5em;
-    font-size: clamp(2rem, 4vw, 2.5rem);
-  }
-  small {
-    display: flex;
-    flex-wrap: wrap;
-    font-size: clamp(0.9rem, 2vw, 1.1rem);
-  }
-  header :global(small:not(:first-of-type)) {
-    margin-top: 0.5em;
-  }
-  small:first-of-type :not(:last-child)::after {
-    content: '~';
-    margin: 0 0.5em;
-    color: var(--mauss-secondary);
-    font-weight: bolder;
-  }
-  small > :global(span) {
-    margin-bottom: 0.25em;
-  }
+	h1 {
+		margin: 1em 0 0.5em;
+		font-size: clamp(2rem, 4vw, 2.5rem);
+	}
+	small {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		font-size: clamp(0.9rem, 2vw, 1.1rem);
+	}
+	header :global(small:not(:first-of-type)) {
+		margin-top: 0.5em;
+	}
+	small:first-of-type :not(:last-child)::after {
+		content: '~';
+		margin: 0 0.5em;
+		color: var(--mauss-secondary);
+		font-weight: bolder;
+	}
+	small > :global(span) {
+		margin-bottom: 0.25em;
+	}
 </style>
