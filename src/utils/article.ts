b3d439ebc10039a21ac63bbb53759fe239efe3ba
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-12 21:06:21 +0700

    Fix fillSiblings to check for optional breakpoint

--- src/utils/article.ts
+++ src/utils/article.ts
@@ -3,7 +3,7 @@ export function fillSiblings(articles: Array<Article>, base: string, breakpoint?
 		if (!articles[i]['siblings']) articles[i]['siblings'] = {};
 		const [prev, next] = [articles[i - 1], articles[i + 1]];
 		if (prev) articles[i]['siblings']['prev'] = { slug: base + prev.slug, title: prev.title };
-		if (breakpoint(next)) return articles;
+		if (breakpoint && breakpoint(next)) return articles;
 		if (next) articles[i]['siblings']['next'] = { slug: base + next.slug, title: next.title };
 	}
 	return articles;
