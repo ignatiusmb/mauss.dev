3c6fda42e242d2b574784fd473ef45930ada01c0
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-12 22:23:41 +0700

    Fix sieve function to search multiple titles

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,11 +1,13 @@
+const exists = (source, query) => source.toLowerCase().includes(query.toLowerCase());
+const compare = (source, queries) => queries.filter((q) => exists(source, q)).length;
+const check = (source, queries) => compare(source, queries) === queries.length;
+
 export function sieve(query, data) {
 	const queries = query.split(' ').filter((q) => q);
 	return data.filter((post) => {
 		const { title } = post;
-		for (const q of queries) {
-			if (title.toLowerCase().includes(q.toLowerCase())) continue;
-			else return;
-		}
-		return post;
+		if (typeof title !== 'string') {
+			for (const key in title) if (check(title[key], queries)) return post;
+		} else if (check(title, queries)) return post;
 	});
 }
