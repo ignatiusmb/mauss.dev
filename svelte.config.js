4efa9b77786487837d76b2293d620bb397837bb7
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-15 13:20:14 +0700

    Fix postcss config in preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,6 @@ import sveltePreprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
 export const preprocess = sveltePreprocess({
-	postcss: { plugins: require('autoprefixer') },
+	postcss: { plugins: [require('autoprefixer')()] },
 	typescript: { transpileOnly: dev },
 });
