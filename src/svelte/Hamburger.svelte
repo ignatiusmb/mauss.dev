b28ca495d6199b6da8b74f3c1e38dc080e2d43fc
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-24 20:21:24 +0700

    Add Hamburger component

---
+++ src/svelte/Hamburger.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let opened = false;
+	import Icon from '@ignatiusmb/elements/svelte/Icon.svelte';
+</script>
+
+<span on:click={() => (opened = !opened)}>
+	<Icon name={opened ? 'close' : 'menu'} />
+</span>
+
+<style>
+	span {
+		cursor: pointer;
+	}
+</style>
