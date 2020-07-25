9b987b9e812e9ed0e2b906455e5b45d8ce54c0e6
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-25 20:27:46 +0700

    Add HelpSign component for Navigation

---
+++ src/svelte/HelpSign.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Link from '@ignatiusmb/elements/svelte/Link.svelte';
+	import Icon from '@ignatiusmb/elements/svelte/Icon.svelte';
+</script>
+
+<Link href="help" inherit>
+	<Icon name="help" />
+</Link>
