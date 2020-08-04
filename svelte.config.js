42f03d6b4f1e5f0fea1c1d13de2a90abd867ac9b
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-08-05 01:19:47 +0700

    Remove unused typescript config and add sourceMap

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,5 @@ import sveltePreprocess from 'svelte-preprocess';
 const dev = process.env.NODE_ENV === 'development';
 export const preprocess = sveltePreprocess({
 	postcss: { plugins: [require('autoprefixer')()] },
-	typescript: { transpileOnly: dev },
+	sourceMap: dev,
 });
