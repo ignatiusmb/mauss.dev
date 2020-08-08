c2afc66f1c9fab4e3db4f7a25119db8e206c329e
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-08-08 13:38:06 +0700

    Upgrade sapper to 0.28

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
-const { mdsvex } = require('mdsvex');
 
-const dev = process.env.NODE_ENV === 'development';
-const preprocess = [
-	sveltePreprocess({
-		postcss: { plugins: [require('autoprefixer')()] },
-		sourceMap: dev,
-	}),
-	mdsvex(),
-];
-
-module.exports = { preprocess };
+// This file is only for language server and svelte-check
+module.exports = {
+	preprocess: sveltePreprocess({ sourceMap: true }),
+};
