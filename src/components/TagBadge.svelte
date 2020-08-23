70aba57d718d3bf844042df2361f41f8f8fb3e08
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-08-23 19:15:37 +0700

    Add more tag badge colors

--- src/components/TagBadge.svelte
+++ src/components/TagBadge.svelte
@@ -1,52 +1,72 @@
 <script>
-  export let tag;
+	export let tag;
 </script>
 
 <span class={tag}>#{tag}</span>
 
 <style>
-  span {
-    padding: 0.2em 0.4em;
-    border-radius: 0.2em;
-    font-size: 0.9rem;
-    background: #d6d9e0;
-    color: #606570;
-  }
+	span {
+		padding: 0.2em 0.4em;
+		border-radius: 0.2em;
+		font-size: 0.9rem;
+		background: #d6d9e0;
+		color: #606570;
+	}
 
-  .android {
-    background: #56a036;
-    color: #fff;
-  }
-  .firebase {
-    background: #ffca28;
-    color: #757575;
-  }
-  .git {
-    background: #f54d27;
-    color: #413932;
-  }
-  .javascript {
-    background: #f7df1e;
-    color: #000;
-  }
-  .lighthouse {
-    background: #304ffe;
-    color: #fff;
-  }
-  .ppl2020 {
-    background: #feb229;
-    color: #000;
-  }
-  .sapper {
-    background: rgb(21, 151, 148);
-    color: #fff;
-  }
-  .svelte {
-    background: #ff3e00;
-    color: #fff;
-  }
-  .tutorial {
-    background: #feffa5;
-    color: #b30047;
-  }
+	.android {
+		background: #56a036;
+		color: #fff;
+	}
+	.firebase {
+		background: #ffca28;
+		color: #757575;
+	}
+	.flutter {
+		background: #54c5f8;
+		color: #003b6c;
+	}
+	.git {
+		background: #f54d27;
+		color: #413932;
+	}
+	.github {
+		background: #24292e;
+		color: #fff;
+	}
+	.javascript {
+		background: #f7df1e;
+		color: #000;
+	}
+	.lighthouse {
+		background: #304ffe;
+		color: #fff;
+	}
+	.nodejs {
+		background: #90c53f;
+		color: #46483d;
+	}
+	.ppl2020 {
+		background: #feb229;
+		color: #000;
+	}
+	.python {
+		background: #3879ab;
+		color: #ffda5d;
+	}
+	.sapper {
+		background: rgb(21, 151, 148);
+		color: #fff;
+	}
+	.svelte {
+		background: #ff3e00;
+		color: #fff;
+	}
+	.tutorial {
+		background: #feffa5;
+		color: #b30047;
+	}
+	.typescript {
+		background: #2775c3;
+		color: #fff;
+	}
 </style>
