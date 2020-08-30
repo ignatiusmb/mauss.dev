de71f90efdf3fe06f2520eb47cf2723e6152ef21
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-08-31 01:16:23 +0700

    Add curated page slice store

--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const mobile = writable(false);
+export const cSlice = writable(0);
 export const pSlice = writable(0);
 export const rSlice = writable(0);
