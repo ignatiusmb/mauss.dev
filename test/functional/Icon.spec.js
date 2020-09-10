916f6933f8950e40f07b9b434b1d37baf6446fbb
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-09-10 23:47:35 +0700

    Test: Remove nonexistent files and pass current helper

--- test/functional/Icon.spec.js
+++
@@ -1,6 +0,0 @@
-import Icon from '../../src/svelte/Icon.svelte';
-import { render, fireEvent } from '@testing-library/svelte';
-
-describe('pagination', () => {
-	it('show', () => {});
-});
--- test/functional/ToggleTheme.spec.js
+++
@@ -1,6 +0,0 @@
-import ToggleTheme from '../../src/svelte/ToggleTheme.svelte';
-import { render, fireEvent } from '@testing-library/svelte';
-
-it('change html class to dark', async () => {
-	const { queryByTitle } = render(ToggleTheme);
-});
