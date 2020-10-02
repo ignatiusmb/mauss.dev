feae8af7f1b77cea862533736f643cb7a60cc2fa
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-10-03 00:04:56 +0700

    Add lambda function to count page hits

---
+++ api/hit.js
@@ -0,0 +1,17 @@
+import { Client, query as q } from 'faunadb';
+const client = new Client({ secret: process.env.FAUNADB_SECRET });
+
+module.exports = async (req, res) => {
+	const { slug } = req.query;
+	if (!slug) return res.status(400).json({ message: 'Slug not provided' });
+
+	// Create new doc if it does not exist
+	if (!(await client.query(q.Exists(q.Match(q.Index('hits_by_slug'), slug)))))
+		await client.query(q.Create(q.Collection('hits'), { data: { slug: slug, hits: 0 } }));
+
+	// Fetch the document for-real and increment hit
+	const document = await client.query(q.Get(q.Match(q.Index('hits_by_slug'), slug)));
+	await client.query(q.Update(document.ref, { data: { hits: document.data.hits + 1 } }));
+
+	return res.status(200).json({ hits: document.data.hits });
+};
