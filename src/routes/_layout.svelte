407f6d9a7217ab67b6ca83199719aa97324edcd2
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-11-15 23:30:28 +0700

    refactor: remove preloading check in layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,7 +2,7 @@
 	export let segment;
 
 	import { stores } from '@sapper/app';
-	const { preloading, page } = stores();
+	const { page } = stores();
 
 	import { ScrollTop } from '@ignatiusmb/elements';
 	import Navigation from '../components/Navigation.svelte';
@@ -15,7 +15,7 @@
 	import '../styles/blog.css';
 	import '../styles/animation.css';
 
-	$: if (!process.dev && process.browser && !$page.error && !$preloading) {
+	$: if (!process.dev && process.browser && !$page.error) {
 		fetch(`api/page?slug=${$page.path}`, { method: 'POST' });
 	}
 </script>
