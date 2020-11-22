91b7991f818b488f050711471aff5e2d263965e6
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-11-23 00:18:49 +0700

    fix: remove love counter

--- src/routes/reviews/[category]/[slug].svelte
+++ src/routes/reviews/[category]/[slug].svelte
@@ -35,7 +35,7 @@
 	canonical="reviews/{post.slug}"
 	title={title.short ? title.short : title.jp ? title.jp : title.en} />
 
-<Article {post} header counter path="content/reviews/{post.slug}.md" {siblings}>
+<Article {post} header path="content/reviews/{post.slug}.md" {siblings}>
 	<div slot="header">
 		<ReviewBanner {post} />
 
