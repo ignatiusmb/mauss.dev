de88c1c29bc93e787c53bf0200c5a1a16e34857b
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-12-17 22:11:22 +0700

    change default store value

--- src/utils/stores.js
+++ src/utils/stores.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const page = writable(null);
-export const cSlice = writable(0);
-export const pSlice = writable(0);
-export const rSlice = writable(0);
+export const cSlice = writable([]);
+export const pSlice = writable([]);
+export const rSlice = writable([]);
