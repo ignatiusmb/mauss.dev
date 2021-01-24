9f892d520f4cacd701808b79b0dd326f150e3e1d
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-01-25 00:30:01 +0700

    fix: page store and add mobile readable

--- src/utils/stores.ts
+++ src/utils/stores.ts
@@ -1,7 +1,12 @@
 import type { Curated, Post, Review } from './types';
-import { writable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
 
-export const page = writable<unknown>(null);
+export const page = writable<Promise<{ loves: number }> | null>(null);
 export const cSlice = writable<Curated[]>([]);
 export const pSlice = writable<Post[]>([]);
 export const rSlice = writable<Review[]>([]);
+
+export const mobile = readable(false, (set) => {
+	const browser = typeof window !== 'undefined';
+	set(browser && window.matchMedia('only screen and (max-width: 760px)').matches);
+});
