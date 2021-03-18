c2dee705d711dedd8378aaea8f61dcdf9e430283
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-18 13:55:06 +0700

    move global.d.ts to src/globals.d.ts

---
+++ src/globals.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+
+declare module '@ignatiusmb/aqua';
+declare module '@ignatiusmb/aqua/lib/aqua.cbs';
