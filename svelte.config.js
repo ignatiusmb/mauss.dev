d07e30e1fa777776f081f60d2b71cf5dd2000755
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-18 13:11:50 +0700

    packages: add vite + kit + adapter-static

--- svelte.config.js
+++
@@ -1,6 +0,0 @@
-const sveltePreprocess = require('svelte-preprocess');
-
-// This file is only for language server and svelte-check
-module.exports = {
-	preprocess: sveltePreprocess({ sourceMap: true }),
-};
