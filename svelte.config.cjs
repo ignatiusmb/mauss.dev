477cccb514df704cdb6923eff5772a3cc5f1a9f3
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-22 17:28:56 +0700

    chore: merge vite config into svelte config file

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,16 +1,25 @@
 const sveltePreprocess = require('svelte-preprocess');
+const adapter = require('@sveltejs/adapter-static');
+const pkg = require('./package.json');
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
 	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: '@sveltejs/adapter-static',
-
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte',
+		vite: () => ({
+			optimizeDeps: {
+				exclude: ['mauss', 'svelement', 'svelement/icons'],
+			},
+			ssr: {
+				noExternal: [
+					'@ignatiusmb/aqua',
+					'svelement',
+					'svelement/icons',
+					...Object.keys(pkg.dependencies || {}),
+				],
+			},
+		}),
 	},
 };
