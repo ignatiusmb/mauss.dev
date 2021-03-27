e99a23c69363957772af10c16b7d0e3d7fca54a5
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-27 15:04:24 +0700

    fix: run dev server by upgrading packages

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -11,13 +11,7 @@ module.exports = {
 				noExternal: [
 					'@ignatiusmb/aqua',
 					'@ignatiusmb/aqua/lib/aqua.cbs',
-					'mauss/api',
-					'mauss/guards',
-					'mauss/utils',
 					'marqua',
-					'svelement',
-					'svelement/icons',
-					'svelement/icons/feather',
 					...Object.keys(pkg.dependencies || {}),
 				],
 			},
