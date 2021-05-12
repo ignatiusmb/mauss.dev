a122eeeb66fdca1d40416076051359907267d80b
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-12 17:20:59 +0700

    simplify locals assignments

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,6 +2,8 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ request, render }) => {
 	const { path } = request;
-	request.locals.entry = `content/src/${path.slice(1, path.lastIndexOf('.'))}`;
+	request.locals = {
+		entry: `content/src/${path.slice(1, path.lastIndexOf('.'))}`,
+	};
 	return await render(request);
 };
