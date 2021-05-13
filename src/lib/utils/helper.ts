31e14c931e89d6b374186268dcbfa05fb6f62e5c
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-13 11:09:28 +0700

    fix: last updated not sorted correctly

--- src/lib/utils/helper.ts
+++ src/lib/utils/helper.ts
@@ -18,11 +18,11 @@ export function createPrettyDate(date: string | Date | undefined): PrettyDate | 
 export function sortCompare<T extends Record<string, any>>(x: T, y: T): number {
 	if (x.date && y.date) {
 		if (typeof x.date === 'string' && typeof y.date === 'string')
-			if (x.date !== y.date) return compare.string(x.date, y.date);
+			if (x.date !== y.date) return compare.date(x.date, y.date);
 		const { updated: xu = '', published: xp = '' } = x.date;
 		const { updated: yu = '', published: yp = '' } = y.date;
-		if (xu && yu && xu !== yu) return compare.string(xu, yu);
-		if (xp && yp && xp !== yp) return compare.string(xp, yp);
+		if (xu && yu && xu !== yu) return compare.date(xu, yu);
+		if (xp && yp && xp !== yp) return compare.date(xp, yp);
 	}
 
 	if (x.released && y.released && x.released !== y.released)
