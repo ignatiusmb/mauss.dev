6c9afbc041b512f1dce98678335e420d714eb6d5
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-21 11:29:15 +0700

    remove unnecessary return

--- src/routes/quotes.json.ts
+++ src/routes/quotes.json.ts
@@ -12,7 +12,6 @@ export const get: RequestHandler<Locals> = async ({ locals: { entry } }) => {
 			const [quote, from] = line.split('#!/');
 			body.push({ author, quote, from });
 		}
-		return undefined;
 	});
 	return { body };
 };
