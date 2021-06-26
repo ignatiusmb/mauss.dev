98fb75fe16b080355138402349380f95f66b8e9f
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-06-26 17:00:27 +0700

    fix: guard entry in handle hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,7 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ request, resolve }) => {
 	const { path } = request;
 	request.locals = {
-		entry: `content/src/${path.slice(1, path.lastIndexOf('.'))}`,
+		entry: path.includes('.') && `content/src/${path.slice(1, path.lastIndexOf('.'))}`,
 	};
 	return await resolve(request);
 };
