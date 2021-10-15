76e5f148e0cad7157ec8658225b2210a4d250a83
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-10-15 16:42:22 +0700

    add error stack to error page on dev mode

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <script>
-	export let status, error;
+	/** @type {number} */
+	export let status;
+	/** @type {Error} */
+	export let error;
+
+	import { dev } from '$app/env';
 </script>
 
 <svelte:head>
@@ -13,10 +18,16 @@
 <article>
 	<h1>{status}</h1>
 	<p>{error.message}</p>
+	{#if dev}
+		<pre>{error.stack}</pre>
+	{/if}
 </article>
 
 <style>
 	article {
+		display: grid;
+		gap: 1em;
+		justify-content: center;
 		margin-top: auto;
 		text-align: center;
 	}
@@ -27,5 +38,8 @@
 	}
 	p {
 		font-size: 2em;
+	}
+	pre {
+		text-align: left;
 	}
 </style>
