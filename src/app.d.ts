c01fe7edb9c20fc4e60f27f7da880628ab50a1f5
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-03 12:32:40 +0700

    rename globals to app and add App namespace

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,3 +4,15 @@ declare module '@ignatiusmb/aqua';
 declare module '@ignatiusmb/aqua/lib/aqua.cbs.js';
 declare module 'markdown-it-texmath';
 declare module 'katex';
+
+declare namespace App {
+	interface Locals {
+		entry: string;
+	}
+
+	// interface Platform {}
+
+	// interface Session {}
+
+	// interface Stuff {}
+}
