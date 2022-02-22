1c4a1c5d1920ca6b186ce4635c5726d8405453e5
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-22 14:51:07 +0700

    update trailing slash on rss

--- src/lib/utils/rss.ts
+++ src/lib/utils/rss.ts
@@ -6,7 +6,7 @@ export default function RSS(channel: RSSChannel, items: RSSItem[]): string {
 	const createItem = (item: RSSItem) => `
 		<item>
 			<title>${clean(item.title)}</title>
-			<link>https://${channel.domain}/${clean(item.slug)}</link>
+			<link>https://${channel.domain}/${clean(item.slug)}/</link>
 			<description>${clean(item.description)}</description>
 			<pubDate>${formatPubDate(item.date)}</pubDate>
 		</item>`;
