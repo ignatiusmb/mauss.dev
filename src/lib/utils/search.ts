aad5ab1af78931e7df22f154cf88a28c27867df3
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-03-03 22:05:32 +0700

    Search Module Fixes (#107)
    * destructure inside
    
    * check array directly with includes
    
    * sort seen by last -> completed -> first
    
    * rename internal compare -> cmp

--- src/lib/utils/search.ts
+++ src/lib/utils/search.ts
@@ -1,16 +1,16 @@
 import type { Entries } from 'mauss/typings';
 import type { Child, SieveDict } from '../types';
-import { comparator, compare as c, regexp } from 'mauss';
+import { comparator, compare, regexp } from 'mauss';
 import { truthy } from 'mauss/guards';
 
 const exists = (source: string | any, query: string | any): boolean =>
 	typeof source !== 'string' ? source === query : regexp(query, 'i').test(source);
-const compare = (source: string[] | string, queries: string[]): number =>
+const cmp = (source: string[] | string, queries: string[]): number =>
 	Array.isArray(source)
-		? source.filter((s) => compare(s, queries)).length
+		? source.filter((s) => cmp(s, queries)).length
 		: queries.filter((q) => exists(source, q)).length;
 const check = (source: string[] | string, queries: string[]): boolean =>
-	compare(source, queries) === queries.length;
+	cmp(source, queries) === queries.length;
 
 const cleanSplit = (data: string): string[] => data.split(' ').filter(truthy);
 export const sift = <T extends Child>(query: string, data: T[]): T[] => {
@@ -24,16 +24,17 @@ export const sift = <T extends Child>(query: string, data: T[]): T[] => {
 function sortCompare<T extends Record<string, any>>(x: T, y: T): number {
 	if (x.date && y.date) {
 		if (typeof x.date === 'string' && typeof y.date === 'string')
-			if (x.date !== y.date) return c.date(x.date, y.date);
+			if (x.date !== y.date) return compare.date(x.date, y.date);
 		const { updated: xu = '', published: xp = '' } = x.date;
 		const { updated: yu = '', published: yp = '' } = y.date;
-		if (xu && yu && xu !== yu) return c.date(xu, yu);
-		if (xp && yp && xp !== yp) return c.date(xp, yp);
+		if (xu && yu && xu !== yu) return compare.date(xu, yu);
+		if (xp && yp && xp !== yp) return compare.date(xp, yp);
 	}
 
-	if (x.released && y.released && x.released !== y.released) return c.date(x.released, y.released);
+	if (x.released && y.released && x.released !== y.released)
+		return compare.date(x.released, y.released);
 
-	if (x.author && y.author) return c.string(x.author, y.author);
+	if (x.author && y.author) return compare.string(x.author, y.author);
 
 	return comparator(x, y);
 }
@@ -44,29 +45,36 @@ const sortBy: Record<string, (x: any, y: any) => number> = {
 		const yr = Number.isNaN(+y.rating) ? +!!y.rating : y.rating;
 		return xr === yr ? sortCompare(x, y) : yr - xr;
 	},
-	seen: (x, y) => c.date(x.seen.first, y.seen.first) || sortCompare(x, y),
-	released: (x, y) => c.date(x.released, y.released) || sortCompare(x, y),
-	published: (x, y) => c.date(x.date.published, y.date.published) || sortCompare(x, y),
+	seen: (x, y) =>
+		compare.date(
+			x.seen.last || x.completed || x.seen.first,
+			y.seen.last || y.completed || y.seen.first
+		) || sortCompare(x, y),
+	released: (x, y) => compare.date(x.released, y.released) || sortCompare(x, y),
+	updated: (x, y) =>
+		compare.date(x.date.updated || x.date.published, y.date.updated || y.date.published) ||
+		sortCompare(x, y),
+	published: (x, y) =>
+		compare.date(x.date.published || x.date.updated, y.date.published || y.date.updated) ||
+		sortCompare(x, y),
 };
 
 export const sort = <T extends Child>(type: string, data: T[]): T[] =>
 	type in sortBy ? data.sort(sortBy[type]) : data.sort(sortCompare);
 
-export function sieve<T extends Child & Record<string, any>>(
-	{ sort_by = 'updated', ...dict }: SieveDict,
-	data: T[]
-): T[] {
+export function sieve<T extends Child & Record<string, any>>(meta: SieveDict, data: T[]): T[] {
 	const identical = ['tags', 'genres'];
 	const intersect = ['categories', 'verdict'];
 
+	const { sort_by = 'updated', ...dict } = meta;
 	const entries = Object.entries(dict) as Entries<Required<typeof dict>>;
 	const cleaned = entries.filter(([k, v]) => !intersect.includes(k) && v.length);
 	const category = entries.find(([k, v]) => k === 'categories' && v.length) || [];
 	const verdict = entries.find(([k, v]) => k === 'verdict' && v.length) || [];
 	const checked = entries.filter(([, v]) => v.length).length;
 	const dFilter = (post: T) =>
-		(verdict.length ? compare(post.verdict, verdict[1]) : 1) &&
-		(category.length && !!post.category ? compare(post.category, category[1]) : 1) &&
-		(cleaned.length ? cleaned.some(([k, v]) => identical.includes(k) && compare(post[k], v)) : 1);
+		(verdict.length ? verdict[1].includes(post.verdict) : 1) &&
+		(category.length && !!post.category ? cmp(post.category, category[1]) : 1) &&
+		(cleaned.length ? cleaned.some(([k, v]) => identical.includes(k) && cmp(post[k], v)) : 1);
 	return sort(sort_by, checked ? data.filter(dFilter) : data);
 }
