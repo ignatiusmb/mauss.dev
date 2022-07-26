c2107dc0cddf6bb1b9de92e848f2abcad9a8e1c3
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-07-26 16:11:42 +0700

    svelte-kit vite plugin

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+
+	server: {
+		port: 3000,
+	},
+};
+
+export default config;
