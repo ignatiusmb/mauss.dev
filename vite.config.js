eba4dadd3d36d57423ab82097de969f2f57d85c6
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-08-03 16:10:36 +0700

    upgrade and minor cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
+export default defineConfig({
 	plugins: [sveltekit()],
 
 	server: {
 		port: 3000,
 	},
-};
-
-export default config;
+});
