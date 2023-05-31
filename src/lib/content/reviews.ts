95d8866e19df2761862a0dd417e7a1b19ddbc314
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-05-31 19:27:11 +0700

    move out of experimental

--- src/lib/content/reviews.ts
+++ src/lib/content/reviews.ts
@@ -146,8 +146,11 @@ export function get(category: string, slug: string) {
 
 function contentParser<T extends Record<string, any>>(data: T, content: string): string {
 	const traverse = (meta: T | string, properties: string): string => {
-		for (const key of properties.split(':'))
-			if (typeof meta !== 'string') meta = meta[Number.isNaN(Number(key)) ? key : Number(key)];
+		for (const key of properties.split(':')) {
+			if (typeof meta !== 'string') {
+				meta = meta[Number.isNaN(Number(key)) ? key : Number(key)];
+			}
+		}
 		return meta as string;
 	};
 
