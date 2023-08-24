7e6ba43ddfac286440b599c85719cd2dc6e0c1d4
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-24 12:02:28 +0700

    pure css togglable nav

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,116 +1,184 @@
 <script lang="ts">
 	import * as feather from 'syv/icons/feather';
 	import Feather from 'syv/icons/Feather.svelte';
-	import NavGrid from './NavGrid.svelte';
-	import NavLink from './NavLink.svelte';
-	import { navigating, page } from '$app/stores';
 
-	export let scrolled = 0;
-
-	let innerWidth: number;
-	let opened = false;
-	$: path = $page.url.pathname.split('/')[1];
-	$: opened = $navigating ? false : opened;
+	import { page } from '$app/stores';
 </script>
 
-<svelte:window bind:innerWidth bind:scrollY={scrolled} />
-
-<nav class:scrolled>
-	<button on:click={() => (opened = !opened)}>
-		{#if opened}
-			<Feather icon={feather.X} />
-		{:else}
-			<Feather icon={feather.Menu} />
-		{/if}
-	</button>
-
-	<NavLink>
+<nav>
+	<a href="/">
 		<img src="/favicon.ico" alt="Alchemauss" width="24" height="24" />
-	</NavLink>
+	</a>
 
-	{#if innerWidth > 600 || opened}
-		<NavGrid>
+	<input type="checkbox" id="menu" />
+	<label for="menu">
+		<Feather icon={feather.Menu} />
+		<Feather icon={feather.X} />
+	</label>
+
+	<menu>
+		<div class="routes">
 			{#each ['about', 'curated', 'posts', 'reviews'] as to}
-				<NavLink {path} {to} hover>{to}</NavLink>
-			{/each}
-		</NavGrid>
-	{/if}
+				{@const current = $page.url.pathname.split('/')[1] === to}
 
-	<a href="/uses" aria-label="Uses page">
-		<Feather icon={feather.Bookmark} />
-	</a>
-	<a href="/rss.xml" aria-label="Get RSS">
-		<Feather icon={feather.Rss} />
-	</a>
-	<a href="/help/" aria-label="See help page">
-		<Feather icon={feather.HelpCircle} />
-	</a>
+				<a href="/{to}" aria-current={current ? 'page' : null}>{to}</a>
+			{/each}
+		</div>
+
+		<div class="shortcuts">
+			<a href="/uses" aria-label="Uses page">
+				<Feather icon={feather.Bookmark} />
+			</a>
+			<a href="/rss.xml" aria-label="Get RSS">
+				<Feather icon={feather.Rss} />
+			</a>
+			<a href="/help/" aria-label="See help page">
+				<Feather icon={feather.HelpCircle} />
+			</a>
+		</div>
+	</menu>
 </nav>
 
 <style>
 	nav :global(a),
 	nav :global(a:focus),
 	nav :global(a:hover) {
+		border-radius: 0.375rem;
 		color: var(--fg-surface);
+	}
+	nav :global(a:focus),
+	nav :global(a:hover) {
+		/* outline: 1px solid var(--theme-primary); */
+		outline: 2px solid var(--bg-cover);
+		/* background: var(--bg-cover); */
 	}
 
 	nav {
 		z-index: 3;
 		width: 100%;
-		position: fixed;
-		bottom: 0;
+		position: sticky;
+		top: 0;
 
 		display: flex;
-		flex-direction: row-reverse;
 		align-items: center;
-		padding: 0.8em 1em;
+		justify-content: space-between;
 
 		transition: var(--t-duration) var(--t-function);
 		background-color: var(--bg-surface);
 		font-family: var(--mrq-heading);
+		color: var(--fg-surface);
 	}
 
-	nav > button,
-	nav > :global(a) {
+	nav > :not(menu) {
+		padding: 0.875rem;
+	}
+
+	input {
+		visibility: hidden;
+		width: auto;
+	}
+	label[for='menu'] {
 		display: inline-flex;
 	}
+	input:not(:checked) + label[for='menu'] > :global(:last-child),
+	input:checked + label[for='menu'] > :global(:first-child) {
+		display: none;
+	}
 
-	nav > :global(a:first-of-type) {
+	menu {
+		display: none;
 		padding: 0;
-		margin-left: auto;
-		margin-right: 1em;
+		margin: 0;
 	}
-	nav > :global(:nth-last-child(2)) {
-		margin-left: 1em;
-		margin-right: 1em;
+	input:checked ~ menu {
+		top: 100%;
+		position: absolute;
+		width: 100%;
+		display: grid;
+
+		border-bottom: 1px solid var(--bg-cover);
+		border-bottom-right-radius: 0.5rem;
+		border-bottom-left-radius: 0.5rem;
+
+		background-color: var(--bg-surface);
+	}
+	input:checked ~ menu .routes {
+		display: grid;
+		gap: 0.375rem;
+		align-items: center;
+		padding: 0.875rem 0.5rem 0;
+	}
+	input:checked ~ menu :global(a) {
+		padding: 0.375rem;
+	}
+	input:checked ~ menu .shortcuts {
+		display: grid;
+		gap: 1em;
+		grid-auto-flow: column;
+		padding: 0.875rem 0.5rem 0.5rem;
+	}
+	input:checked ~ menu .shortcuts a {
+		width: 100%;
+		display: inline-flex;
+		justify-content: center;
 	}
 
-	@media only screen and (max-width: 599px) {
-		nav {
-			border-top: 0.25em solid var(--theme-primary);
-			box-shadow: 0 4px 3px rgba(0, 0, 0, 0.5);
-			transition: var(--t-duration) var(--t-function);
-			background-color: var(--bg-surface);
-		}
-	}
 	@media only screen and (min-width: 600px) {
-		nav {
-			position: sticky;
-			top: 0;
-			flex-direction: row;
-			border-top: none;
-			background-color: var(--bg-base);
-		}
-		nav.scrolled {
-			box-shadow: 0 4px 3px rgba(0, 0, 0, 0.5);
-			transition: var(--t-duration) var(--t-function);
-			background-color: var(--bg-surface);
-		}
-		nav > button:first-child {
+		input,
+		label[for='menu'] {
 			display: none;
 		}
-		nav > button:first-child + :global(a) {
-			margin: 0;
+
+		menu {
+			width: 100%;
+			display: flex;
+			/* align-items: center; */
+			justify-content: space-between;
+		}
+
+		.routes {
+			display: grid;
+			gap: 0.5em;
+			grid-auto-flow: column;
+			align-items: center;
+		}
+		.routes a[aria-current]::after {
+			width: 100%;
+			background-color: var(--theme-primary);
+		}
+		.routes a {
+			position: relative;
+			padding: 0.1em 0.2em;
+			color: var(--fg-surface);
+			text-transform: capitalize;
+		}
+		.routes a::after {
+			content: '';
+			position: absolute;
+			right: 0;
+			bottom: 0;
+			width: 0;
+			height: 0.15em;
+			border-radius: 0.25em;
+			background-color: var(--theme-secondary);
+			transition: width var(--t-duration) ease;
+			transform: translateY(100%);
+		}
+		.routes a:hover::after,
+		.routes a:focus::after {
+			left: 0;
+			right: auto;
+			width: 100%;
+		}
+
+		.shortcuts {
+			display: grid;
+			grid-auto-flow: column;
+			align-items: center;
+		}
+		.shortcuts a {
+			display: inline-flex;
+			padding: 0.875rem 0.5rem;
 		}
 	}
 </style>
