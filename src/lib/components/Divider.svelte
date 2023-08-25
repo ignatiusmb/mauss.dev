0697f0144704ea0cb2d81e04abaecdd6df7dd388
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-25 12:16:10 +0700

    more nav adjustments

---
+++ src/lib/components/Divider.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let type: 'horizontal' | 'vertical';
+</script>
+
+<div class="divider {type}" />
+
+<style>
+	div {
+		padding: 0;
+		background: var(--bg-cover);
+	}
+	.horizontal {
+		width: 100%;
+		height: 1px;
+	}
+	.vertical {
+		width: 1px;
+		height: 100%;
+	}
+</style>
--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import * as feather from 'syv/icons/feather';
 	import Feather from 'syv/icons/Feather.svelte';
+	import Divider from '$lib/components/Divider.svelte';
 
 	import { page } from '$app/stores';
+
+	let opened = false;
 </script>
 
 <nav>
@@ -10,7 +13,7 @@
 		<img src="/favicon.ico" alt="Alchemauss" width="24" height="24" />
 	</a>
 
-	<input type="checkbox" id="menu" />
+	<input type="checkbox" id="menu" bind:checked={opened} />
 	<label for="menu">
 		<Feather icon={feather.Menu} />
 		<Feather icon={feather.X} />
@@ -19,10 +22,12 @@
 	<menu>
 		<div class="routes">
 			{#each ['about', 'curated', 'posts', 'reviews'] as to}
-				{@const current = $page.url.pathname.split('/')[1] === to}
+				{@const current = $page.url.pathname.startsWith(`/${to}`)}
 
 				<a href="/{to}" aria-current={current ? 'page' : null}>{to}</a>
 			{/each}
+
+			<Divider type="horizontal" />
 		</div>
 
 		<div class="shortcuts">
@@ -40,19 +45,6 @@
 </nav>
 
 <style>
-	nav :global(a),
-	nav :global(a:focus),
-	nav :global(a:hover) {
-		border-radius: 0.375rem;
-		color: var(--fg-surface);
-	}
-	nav :global(a:focus),
-	nav :global(a:hover) {
-		/* outline: 1px solid var(--theme-primary); */
-		outline: 2px solid var(--bg-cover);
-		/* background: var(--bg-cover); */
-	}
-
 	nav {
 		z-index: 3;
 		width: 100%;
@@ -63,14 +55,24 @@
 		align-items: center;
 		justify-content: space-between;
 
-		transition: var(--t-duration) var(--t-function);
 		background-color: var(--bg-surface);
 		font-family: var(--mrq-heading);
 		color: var(--fg-surface);
 	}
-
+	a {
+		outline: 2px solid transparent;
+		transition-duration: var(--t-duration);
+	}
+	nav a:focus {
+		outline-color: var(--bg-cover);
+	}
+	nav > a:first-child {
+		flex-shrink: 0;
+		border-radius: 0.875rem;
+		outline-offset: -0.5rem;
+	}
 	nav > :not(menu) {
-		padding: 0.875rem;
+		padding: 1rem 0.875rem;
 	}
 
 	input {
@@ -86,73 +88,90 @@
 	}
 
 	menu {
-		display: none;
-		padding: 0;
-		margin: 0;
-	}
-	input:checked ~ menu {
-		top: 100%;
+		visibility: hidden;
+		z-index: 2;
+		top: 0;
 		position: absolute;
 		width: 100%;
 		display: grid;
 
+		padding: 0;
+		margin: 0;
 		border-bottom: 1px solid var(--bg-cover);
 		border-bottom-right-radius: 0.5rem;
 		border-bottom-left-radius: 0.5rem;
+		box-shadow: 0.25rem 0.25rem 0.25rem rgba(0, 0, 0, 0.188);
 
 		background-color: var(--bg-surface);
+		transition-duration: calc(var(--t-duration) * 2);
+		transition-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+		/* transition-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1); */
+		transform: translate3d(0, -100%, 0) scale3d(0.9, 0.9, 1);
 	}
-	input:checked ~ menu .routes {
+	input:checked ~ menu {
+		visibility: visible;
+		top: 100%;
+		transform: none;
+	}
+	menu a {
+		padding: 0.375rem;
+		border-radius: 0.375rem;
+		color: var(--fg-surface);
+		text-transform: capitalize;
+	}
+	menu .routes {
 		display: grid;
 		gap: 0.375rem;
 		align-items: center;
-		padding: 0.875rem 0.5rem 0;
+		padding: 0 0.5rem;
 	}
-	input:checked ~ menu :global(a) {
-		padding: 0.375rem;
-	}
-	input:checked ~ menu .shortcuts {
+	menu .shortcuts {
 		display: grid;
 		gap: 1em;
 		grid-auto-flow: column;
-		padding: 0.875rem 0.5rem 0.5rem;
+		padding: 0.5rem;
 	}
-	input:checked ~ menu .shortcuts a {
+	menu .shortcuts a {
 		width: 100%;
 		display: inline-flex;
 		justify-content: center;
 	}
 
-	@media only screen and (min-width: 600px) {
+	/* change to 600 when there's more routes */
+	@media only screen and (min-width: 500px) {
 		input,
 		label[for='menu'] {
 			display: none;
 		}
 
+		input:checked ~ menu,
 		menu {
+			visibility: unset;
+			position: relative;
 			width: 100%;
 			display: flex;
-			/* align-items: center; */
 			justify-content: space-between;
+
+			border-bottom: none;
+			box-shadow: none;
+			transform: none;
 		}
 
-		.routes {
+		menu .routes {
 			display: grid;
 			gap: 0.5em;
 			grid-auto-flow: column;
 			align-items: center;
 		}
-		.routes a[aria-current]::after {
+		menu .routes a[aria-current]::after {
 			width: 100%;
 			background-color: var(--theme-primary);
 		}
-		.routes a {
+		menu .routes a {
 			position: relative;
-			padding: 0.1em 0.2em;
 			color: var(--fg-surface);
-			text-transform: capitalize;
 		}
-		.routes a::after {
+		menu .routes a::after {
 			content: '';
 			position: absolute;
 			right: 0;
@@ -164,21 +183,15 @@
 			transition: width var(--t-duration) ease;
 			transform: translateY(100%);
 		}
-		.routes a:hover::after,
-		.routes a:focus::after {
+		menu .routes a:hover::after,
+		menu .routes a:focus::after {
 			left: 0;
 			right: auto;
 			width: 100%;
 		}
 
-		.shortcuts {
-			display: grid;
-			grid-auto-flow: column;
-			align-items: center;
-		}
-		.shortcuts a {
-			display: inline-flex;
-			padding: 0.875rem 0.5rem;
+		menu .shortcuts {
+			gap: 0.375rem;
 		}
 	}
 </style>
