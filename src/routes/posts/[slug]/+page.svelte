1aa2bff00fce9ebcf4dc091681b1e3bab830e8fb
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-10-23 19:14:09 +0700

    formatting

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -21,29 +21,16 @@
 
 	{#if data.article.title.startsWith('Accessibility!')}
 		<blockquote>
-			<p>
-				Making the web accessible isn't doing anyone a favor, it's you doing your job properly as a
-				web developer, in the front end.
-			</p>
+			<!-- prettier-ignore -->
+			<p>Making the web accessible isn't doing anyone a favor, it's you doing your job properly as a web developer, in the front end.</p>
 		</blockquote>
 
 		<section class="info-box warning">
-			<p>
-				<strong>Accessible Rich Internet Applications (ARIA)</strong>
-				is a set of attributes that define ways to make web content and web applications (especially
-				those developed with JavaScript) more accessible to people with disabilities.
-			</p>
+			<!-- prettier-ignore -->
+			<p><strong>Accessible Rich Internet Applications (ARIA)</strong> is a set of attributes that define ways to make web content and web applications (especially those developed with JavaScript) more accessible to people with disabilities.</p>
 
-			<p>
-				Many of these widgets were later incorporated into HTML5, and
-				<strong>
-					developers should prefer using the correct semantic HTML element over using ARIA,
-				</strong>
-				if such an element exists. For instance, native elements have built-in keyboard accessibility,
-				roles and states. However, if you choose to use ARIA, you are responsible for mimicking (the
-				equivalent) browser behavior in script. Source:
-				<Link href="https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA">MDN</Link>
-			</p>
+			<!-- prettier-ignore -->
+			<p>Many of these widgets were later incorporated into HTML5, and <strong>developers should prefer using the correct semantic HTML element over using ARIA,</strong> if such an element exists. For instance, native elements have built-in keyboard accessibility, roles and states. However, if you choose to use ARIA, you are responsible for mimicking (the equivalent) browser behavior in script. Source: <Link href="https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA">MDN</Link></p>
 		</section>
 	{/if}
 
