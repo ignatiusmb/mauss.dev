5f6b300780f2e4ceccf2ea8863df00d47b8f2af5
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-10-27 18:24:02 +0700

    refactor: encapsulate rss file generator

--- src/routes/rss.xml/core.ts
+++ src/routes/rss.xml/core.ts
@@ -1,49 +1,64 @@
-const chars: Record<string, string> = { '"': 'quot', "'": '#39', '&': 'amp', '<': 'lt', '>': 'gt' };
-const formatPubDate = (date: string) => new Date(date).toUTCString();
-const clean = (html: string) => (!html ? '' : html.replace(/["'&<>]/g, (c) => `&${chars[c]};`));
+function escape(literals: TemplateStringsArray, ...inputs: Array<string | string[]>) {
+	const target = /["'&<>]/g;
+	const chars: Record<string, string> = {
+		'"': 'quot',
+		"'": '#39',
+		'&': 'amp',
+		'<': 'lt',
+		'>': 'gt',
+	};
 
-export function RSS(channel: RSSChannel, items: RSSItem[]): string {
-	const createItem = (item: RSSItem) => `
-		<item>
-			<title>${clean(item.title)}</title>
-			<link>https://${channel.domain}/${clean(item.slug)}/</link>
-			<description>${clean(item.description)}</description>
-			<pubDate>${formatPubDate(item.date)}</pubDate>
-		</item>`;
+	let out = literals[0];
+	for (let i = 0; i < inputs.length; i++) {
+		const value = inputs[i];
+		if (typeof value !== 'string') out += value.join('');
+		else out += value.replace(target, (c) => `&${chars[c]};`);
+		out += literals[i + 1];
+	}
+	return out.trim();
+}
 
-	const xml = `
-	<?xml version="1.0" encoding="UTF-8" ?>
+export function RSS(
+	channel: {
+		domain: string;
+		image?: string;
+		title: string;
+		slug?: string;
+		description: string;
+		language?: string;
+	},
+	items: Array<{
+		title: string;
+		slug: string;
+		description: string;
+		date: string;
+	}>,
+): string {
+	const xml = escape`
+	<?xml version="1.0" encoding="UTF-8"?>
 	<rss version="2.0">
 	<channel>
-		<title>${clean(channel.title)}</title>
-		<link>https://${channel.domain}/${clean(channel.slug || '')}</link>
-		<description>${clean(channel.description)}</description>
+		<title>${channel.title}</title>
+		<link>https://${channel.domain}/${channel.slug || ''}</link>
+		<description>${channel.description}</description>
 		<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
 		<language>${channel.language || 'en'}</language>
 		<image>
 			<url>https://${channel.domain}/${channel.image || 'favicon.ico'}</url>
 			<title>${channel.title}</title>
-			<link>https://${channel.domain}/${clean(channel.slug || '')}</link>
+			<link>https://${channel.domain}/${channel.slug || ''}</link>
 		</image>
-		${items.map(createItem).join('')}
+		${items.map(
+			(item) => escape`
+			<item>
+				<title>${item.title}</title>
+				<link>https://${channel.domain}/${item.slug}/</link>
+				<description>${item.description}</description>
+				<pubDate>${new Date(item.date).toUTCString()}</pubDate>
+			</item>`,
+		)}
 	</channel>
 	</rss>`;
 
-	return xml.replace(/>[^\S]+/gm, '>').trim();
-}
-
-interface RSSChannel {
-	domain: string;
-	image?: string;
-	title: string;
-	slug?: string;
-	description: string;
-	language?: string;
-}
-
-export interface RSSItem {
-	title: string;
-	slug: string;
-	description: string;
-	date: string;
+	return xml.replace(/>[^\S]+/gm, '>');
 }
