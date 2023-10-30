90c13becff1c8d38a47cf2f3bdc8a6fbd8492ad0
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-10-30 16:30:44 +0700

    combine header to article

--- src/lib/pages/Article.svelte
+++ src/lib/pages/Article.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 	import ProgressBar from 'syv/core/ProgressBar.svelte';
-	import Header from './Header.svelte';
 
-	import type { ComponentProps } from 'svelte';
 	import { hydrate } from 'marqua/browser';
+	import { dt } from 'mauss';
 	import { navigating } from '$app/stores';
 
 	export let path = '';
-	export let post: null | ComponentProps<Header>['post'] = null;
+	export let post: null | {
+		title: string | { en?: string; jp?: string };
+		date: string | { published?: string; updated?: string };
+		table: Array<{ id: string; title: string }>;
+		estimate: number;
+
+		author?: { name?: string; link?: string; img?: string };
+		description?: string;
+	} = null;
 
 	type Flank = null | { slug: string; title: string | Record<string, any> };
 	export let flank: null | Partial<Record<'back' | 'next', Flank>> = null;
@@ -19,9 +26,37 @@
 
 <main use:hydrate={$navigating}>
 	{#if $$slots.header && post}
-		<Header {post}>
+		<header>
+			<aside>
+				{#if typeof post.date === 'object'}
+					{@const { published, updated } = post.date}
+					<time datetime={updated || published}>
+						{dt.format(updated || published)('DD MMMM YYYY')}
+					</time>
+				{:else}
+					<time datetime={post.date}>{dt.format(post.date)('DD MMMM YYYY')}</time>
+				{/if}
+
+				<span class="dash">&mdash;</span>
+
+				<span>{post.estimate} min read</span>
+			</aside>
+
+			{#if typeof post.title === 'string'}
+				<h1>{post.title}</h1>
+			{:else if post.title.jp}
+				<h1>{post.title.jp}</h1>
+			{:else}
+				<h1>{post.title.en}</h1>
+			{/if}
+
+			<a href={post.author?.link || '/about/'}>
+				<img src={post.author?.img || '/assets/profile/mauss.jpg'} alt="author profile" />
+				<span>{post.author?.name || 'Ignatius Bagussuputra'}</span>
+			</a>
+
 			<slot name="header" />
-		</Header>
+		</header>
 
 		{#if post.table.length}
 			<section id="objective" class="info-box">
@@ -95,6 +130,49 @@
 		word-wrap: break-word;
 		line-height: 1.5;
 	}
+
+	header {
+		z-index: 0;
+		position: relative;
+		display: grid;
+		gap: 0.8rem;
+		justify-items: center;
+		margin-top: 3rem;
+
+		line-height: 1;
+		font-family: var(--mrq-heading);
+	}
+
+	aside,
+	header a {
+		display: grid;
+		gap: 0.5rem;
+		grid-auto-flow: column;
+		align-items: center;
+		font-size: 0.875rem;
+	}
+	header a {
+		grid-template-columns: 1.5rem 1fr;
+		margin: 0.5rem 0;
+		font-size: 1rem;
+	}
+	header a img {
+		border-radius: 50%;
+	}
+
+	h1 {
+		font-size: clamp(2.5rem, 4vw, 3rem);
+		text-align: center;
+		text-wrap: balance;
+	}
+
+	header :global(.dash) {
+		color: var(--theme-secondary);
+		font-weight: 600;
+	}
+
+	/* ---- {@html} ---- */
+
 	main > :global(*) {
 		grid-column: 2;
 	}
