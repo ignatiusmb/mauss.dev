8125a0ad49b4ae78b4dc0c0d99da53eb945d2b50
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-01-11 16:21:20 +0700

    feat: use original asset format

--- src/lib/content/media.ts
+++ src/lib/content/media.ts
@@ -6,16 +6,40 @@ const ROOT = `${process.cwd()}/static/uploads`;
 export function assemble(buffer: Buffer, output: `/${string}`) {
 	const dir = ROOT + output.slice(0, output.lastIndexOf('/'));
 
-	if (/\.(mp4)$/.test(output)) {
-		mkdirSync(dir, { recursive: true });
-		writeFileSync(ROOT + output, buffer);
-		return '/uploads' + output;
+	switch (true) {
+		case /\.(mp4)$/.test(output): {
+			mkdirSync(dir, { recursive: true });
+			writeFileSync(ROOT + output, buffer);
+			return '/uploads' + output;
+		}
+		case /\.(jpe?g)$/.test(output): {
+			mkdirSync(dir, { recursive: true });
+			const path = output.replace(/\.[^/.]+$/, '.jpeg');
+
+			const image = sharp(buffer);
+			image.metadata().then(({ orientation = 0, width, height }) => {
+				const original = orientation >= 5 ? height : width;
+				image.resize(Math.min(original || 1080, 1080));
+				image.jpeg({ mozjpeg: true }).toFile(ROOT + path);
+			});
+
+			return '/uploads' + path;
+		}
+		case /\.(png|svg)$/.test(output): {
+			mkdirSync(dir, { recursive: true });
+			const path = output.replace(/\.[^/.]+$/, '.png');
+
+			const image = sharp(buffer);
+			image.metadata().then(({ orientation = 0, width, height }) => {
+				const original = orientation >= 5 ? height : width;
+				image.resize(Math.min(original || 1080, 1080));
+				image.png({ quality: 85 }).toFile(ROOT + path);
+			});
+
+			return '/uploads' + path;
+		}
+
+		default:
+			return;
 	}
-
-	if (!/\.(jpe?g|png|svg)$/.test(output)) return;
-
-	mkdirSync(dir, { recursive: true });
-	const webp = sharp(buffer).webp();
-	const path = output.replace(/\.[^/.]+$/, '.webp');
-	return webp.toFile(ROOT + path), '/uploads' + path;
 }
