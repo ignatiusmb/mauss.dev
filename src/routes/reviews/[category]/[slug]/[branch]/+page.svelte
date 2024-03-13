144e22668af39c6a1af7aa5a71254f0de2570629
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-03-13 16:59:36 +0700

    commit the new pages

---
+++ src/routes/reviews/[category]/[slug]/[branch]/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Article from '$lib/pages/Article.svelte';
+	import Backdrop from '../Backdrop.svelte';
+	import { page } from '$app/stores';
+
+	export let data;
+</script>
+
+<Article
+	post={data.article}
+	path="sites/dev.mauss/curated/{data.article.slug}/+{$page.params.branch}.md"
+>
+	<svelte:fragment slot="header">
+		<Backdrop post={data.article} />
+
+		{#if data.article.link}
+			<small>
+				<span>[</span>
+				{#each Object.entries(data.article.link) as [key, link], idx}
+					{#if idx !== 0}<span class="dash">&mdash;</span>{/if}
+
+					{#each typeof link === 'string' ? [link] : link as href, v}
+						{@const indexed = typeof link !== 'string' ? ` (${v + 1})` : ''}
+
+						{#if v !== 0}<span class="dash">&mdash;</span>{/if}
+						<a {href}>{key}{indexed}</a>
+					{/each}
+				{/each}
+				<span>]</span>
+			</small>
+		{/if}
+	</svelte:fragment>
+
+	{@html data.article.content}
+</Article>
