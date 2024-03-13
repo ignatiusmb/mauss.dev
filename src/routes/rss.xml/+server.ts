daa4ac33bb003c32aa36105b8268439936c070fb
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-03-13 16:57:35 +0700

    fix rss

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -2,38 +2,37 @@ import { traverse } from 'aubade/compass';
 import { compare } from 'mauss';
 import { RSS, channel } from './builder';
 
-const items = traverse('content/sites/dev.mauss', { depth: -1 }).hydrate(
-	({ breadcrumb, buffer, parse }) => {
-		if (breadcrumb[0].includes('draft')) return;
-		if (!breadcrumb[0].endsWith('.md')) return;
+const items = traverse('content/sites/dev.mauss', { depth: -1 })
+	.hydrate(
+		({ breadcrumb, buffer, parse }) => {
+			const { metadata } = parse(buffer.toString('utf-8'));
+			const { title, date, description: info } = metadata;
 
-		const { metadata } = parse(buffer.toString('utf-8'));
-		const { title, date, description: info } = metadata;
-
-		switch (true) {
-			case breadcrumb.includes('curated') && breadcrumb[0].startsWith('+article'): {
-				const slug = `curated/${breadcrumb[1]}`;
-				const description = `${title} curated by Alchemauss`;
-				return { slug, title, description, date };
+			switch (true) {
+				case breadcrumb.includes('curated'): {
+					const slug = `curated/${breadcrumb[1]}`;
+					const description = `${title} curated by Alchemauss`;
+					return { slug, title, description, date };
+				}
+				case breadcrumb.includes('reviews'): {
+					if (metadata.verdict === 'pending') return;
+					const slug = `reviews/${breadcrumb[2]}/${breadcrumb[1]}`;
+					const name = typeof title === 'string' ? title : title.en;
+					const description = `${name} reviewed by Alchemauss`;
+					return { slug, title: name, description, date };
+				}
+				case breadcrumb.includes('posts'): {
+					const slug = `posts/${breadcrumb[1].replace('.md', '')}`;
+					const description = info || 'A post by Alchemauss';
+					return { slug, title, description, date };
+				}
+				default:
+					return;
 			}
-			case breadcrumb.includes('reviews') && breadcrumb[0].startsWith('+article'): {
-				if (metadata.verdict === 'pending') return;
-				const slug = `reviews/${breadcrumb[2]}/${breadcrumb[1]}`;
-				const name = typeof title === 'string' ? title : title.en;
-				const description = `${name} reviewed by Alchemauss`;
-				return { slug, title: name, description, date };
-			}
-			case breadcrumb.includes('posts'): {
-				const slug = `posts/${breadcrumb[1].replace('.md', '')}`;
-				const description = info || 'A post by Alchemauss';
-				return { slug, title, description, date };
-			}
-			default:
-				return;
-		}
-	},
-	(items) => items.sort((x, y) => compare.date(x.date, y.date) || compare.string(x.title, y.title)),
-);
+		},
+		(path) => path.endsWith('+article.md'),
+	)
+	.sort((x, y) => compare.date(x.date, y.date) || compare.string(x.title, y.title));
 
 export const prerender = true;
 export async function GET() {
