71c179fee5aad18308a1a6f7abec04de815f47c7
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-08-28 15:38:27 +0700

    feat: query string filters (#258)

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -1,24 +1,29 @@
 import type { Schema } from '$content/posts.json/+server.js';
 import type { by } from './search.svelte';
+import { building } from '$app/environment';
+import { scope } from 'mauss';
 
-export async function load({ fetch }) {
+export async function load({ fetch, url }) {
 	const { items, metadata }: Schema = await fetch('/content/posts.json').then((r) => r.json());
 
 	return {
 		list: items,
 		filters: {
-			category: {
-				selected: '',
-				options: metadata.categories.reduce((a, v) => ({ ...a, [v]: v }), {}),
-			},
-			tags: metadata.tags.map((v) => ({ name: v, selected: false })),
-			sort_by: {
-				required: true,
-				selected: 'date' as keyof typeof by,
-				options: {
-					date: 'Date',
-				} satisfies Record<keyof typeof by, string>,
-			},
+			category: scope(() => {
+				const options = metadata.categories.reduce((a, v) => ({ ...a, [v]: v }), {});
+				const selected = (!building && url.searchParams.get('category')) || '';
+				return { options, selected: selected in options ? selected : '' };
+			}),
+			tags: scope(() => {
+				const selected = (!building && url.searchParams.get('tags')?.split(',')) || [];
+				return metadata.tags.map((v) => ({ name: v, selected: selected.includes(v) }));
+			}),
+			sort_by: scope(() => {
+				const options = { date: 'Date' } satisfies Record<keyof typeof by, string>;
+				const sort = (!building && url.searchParams.get('sort_by')) || '';
+				const selected = (sort in options && sort) || 'date';
+				return { required: true, options, selected: selected as keyof typeof by };
+			}),
 		},
 		meta: {
 			canonical: 'posts',
--- src/routes/reviews/+page.server.ts
+++ src/routes/reviews/+page.server.ts
@@ -1,6 +1,7 @@
 import type { Schema } from '$content/reviews.json/+server.js';
 import type { by } from './search.svelte.js';
 import { building } from '$app/environment';
+import { scope } from 'mauss';
 
 export async function load({ fetch, url }) {
 	const { items, metadata }: Schema = await fetch('/content/reviews.json').then((r) => r.json());
@@ -9,31 +10,37 @@ export async function load({ fetch, url }) {
 		list: items,
 		query: (!building && url.searchParams.get('q')) || '',
 		filters: {
-			category: {
-				selected: '',
-				options: metadata.categories.reduce((a, v) => ({ ...a, [v]: v }), {}),
-			},
-			genres: metadata.genres.map((v) => ({ name: v, selected: false })),
-			verdict: {
-				selected: '',
-				options: {
+			category: scope(() => {
+				const options = metadata.categories.reduce((a, v) => ({ ...a, [v]: v }), {});
+				const selected = (!building && url.searchParams.get('category')) || '';
+				return { options, selected: selected in options ? selected : '' };
+			}),
+			genres: scope(() => {
+				const selected = (!building && url.searchParams.get('genres')?.split(',')) || [];
+				return metadata.genres.map((v) => ({ name: v, selected: selected.includes(v) }));
+			}),
+			verdict: scope(() => {
+				const options = {
 					'must-watch': 'Must Watch',
 					recommended: 'Recommended',
 					contextual: 'Contextual',
 					'not-recommended': 'Not Recommended',
 					pending: 'Pending',
-				},
-			},
-			sort_by: {
-				required: true,
-				selected: 'date' as keyof typeof by,
-				options: {
+				};
+				const selected = (!building && url.searchParams.get('verdict')) || '';
+				return { options, selected: selected in options ? selected : '' };
+			}),
+			sort_by: scope(() => {
+				const options = {
 					date: 'Date',
 					rating: 'Rating',
 					seen: 'Last seen',
 					premiere: 'Premiered',
-				} satisfies Record<keyof typeof by, string>,
-			},
+				} satisfies Record<keyof typeof by, string>;
+				const sort = (!building && url.searchParams.get('sort_by')) || '';
+				const selected = (sort in options && sort) || 'date';
+				return { required: true, options, selected: selected as keyof typeof by };
+			}),
 		},
 		meta: {
 			canonical: 'reviews',
