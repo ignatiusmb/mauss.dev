cced5f070af585ecf569d72dfe3014a1c9143374
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-09 09:12:14 +0700

    try this too

--- workspace/atelier/src/routes/reviews/+page.server.ts
+++ workspace/atelier/src/routes/reviews/+page.server.ts
@@ -1,5 +1,4 @@
 import type { by } from './search.svelte.js';
-import { building } from '$app/environment';
 import { scope } from 'mauss';
 
 export async function load({ parent, url }) {
@@ -7,15 +6,15 @@ export async function load({ parent, url }) {
 
 	return {
 		list: items,
-		query: (!building && url.searchParams.get('q')) || '',
+		query: url.searchParams.get('q') || '',
 		filters: {
 			category: scope(() => {
 				const options = metadata.categories.reduce((a, v) => ({ ...a, [v]: v }), {});
-				const selected = (!building && url.searchParams.get('category')) || '';
+				const selected = url.searchParams.get('category') || '';
 				return { options, selected: selected in options ? selected : '' };
 			}),
 			genres: scope(() => {
-				const selected = (!building && url.searchParams.get('genres')?.split('-')) || [];
+				const selected = url.searchParams.get('genres')?.split('-') || [];
 				return metadata.genres.map((v) => ({ name: v, selected: selected.includes(v) }));
 			}),
 			verdict: scope(() => {
@@ -26,7 +25,7 @@ export async function load({ parent, url }) {
 					'not-recommended': 'Not Recommended',
 					pending: 'Pending',
 				};
-				const selected = (!building && url.searchParams.get('verdict')) || '';
+				const selected = url.searchParams.get('verdict') || '';
 				return { options, selected: selected in options ? selected : '' };
 			}),
 			sort_by: scope(() => {
@@ -36,7 +35,7 @@ export async function load({ parent, url }) {
 					seen: 'Last seen',
 					premiere: 'Premiered',
 				} satisfies Record<keyof typeof by, string>;
-				const sort = (!building && url.searchParams.get('sort_by')) || '';
+				const sort = url.searchParams.get('sort_by') || '';
 				const selected = (sort in options && sort) || 'date';
 				return { required: true, options, selected: selected as keyof typeof by };
 			}),
