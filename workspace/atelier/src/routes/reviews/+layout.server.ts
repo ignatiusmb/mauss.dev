2589fe5efa3e0868a2a038e5aee27125687a1ab5
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-09 09:11:46 +0700

    all this time?

--- workspace/atelier/src/routes/reviews/+layout.server.ts
+++ workspace/atelier/src/routes/reviews/+layout.server.ts
@@ -1,5 +1,7 @@
 import type { Schema } from '$content/reviews.json/+server.js';
 
+export const prerender = false;
+
 export async function load({ fetch }) {
 	const data: Schema = await fetch('/content/reviews.json').then((r) => r.json());
 
