9f787d982ce8a5391359e0a464246b463cc94f07
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-26 23:46:48 +0700

    reinstate before build generation

--- workspace/apex/vite.config.js
+++ workspace/apex/vite.config.js
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { DATA } from './src/lib/content';
 
-export default defineConfig(({}) => {
+export default defineConfig(async ({ command }) => {
+	if (command === 'build' /* @ts-expect-error - generate static assets */) {
+		await Promise.all(Object.keys(DATA).map((route) => void DATA[route]()));
+	}
+
 	return {
 		build: {
 			cssTarget: 'chrome111',
