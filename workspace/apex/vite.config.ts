47f34fa95feb2174b18c6a31b51c6fe542014afa
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-30 16:35:57 +0700

    use vite plugin

--- workspace/apex/vite.config.ts
+++ workspace/apex/vite.config.ts
@@ -2,20 +2,24 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { ROUTES } from './src/routes/content/builder';
 
-export default defineConfig(async ({ command }) => {
-	if (command === 'build') {
+const uploads = {
+	name: 'generate-uploads',
+	apply: 'build' as const,
+	async buildStart() {
 		const uploads = await ROUTES['/uploads']();
 		for (const route in uploads) {
 			console.log(`Uploaded ${uploads[route].length} files for ${route}`);
 		}
-	}
+	},
+};
 
+export default defineConfig(() => {
 	return {
 		build: {
 			cssTarget: 'chrome111',
 		},
 
-		plugins: [sveltekit()],
+		plugins: [uploads, sveltekit()],
 
 		server: {
 			fs: { allow: ['..'] },
